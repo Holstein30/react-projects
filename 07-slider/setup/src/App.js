7953908cc5ce7a24a4723cf2c05e8381bf3d987e
Holstein30 <holstein30@live.com>
2021-08-27 09:58:24 -0400

    07 complete

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,56 +3,93 @@ import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 function App() {
-  const [index, setIndex] = useState(0);
-  const { id, image, name, title, quote } = data[index];
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
 
-  const checkNumber = (number) => {
-    if (number > data.length - 1) {
-      return 0;
-    }
-    if (number < 0) {
-      return data.length - 1;
-    }
-    return number;
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
   };
-
-  const nextPerson = () => {
-    setIndex((index) => {
-      let newIndex = index + 1;
-      return checkNumber(newIndex);
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
     });
   };
 
-  const previousPerson = () => {
-    setIndex((index) => {
-      let newIndex = index - 1;
-      return checkNumber(newIndex);
-    });
-  };
+  // useEffect(() => {
+  //   const lastIndex = people.length - 1
+  //   if (index < 0) {
+  //     setIndex(lastIndex)
+  //   }
+  //   if (index > lastIndex) {
+  //     setIndex(0)
+  //   }
+  // }, [index, people])
 
-  const randomPerson = () => {
-    let randNumber = Math.floor(Math.random() * data.length);
-    if (randNumber === index) {
-      let randNumber = index + 1;
-    }
-    setIndex(checkNumber(randNumber));
-  };
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
-    <main>
-      <button onClick={nextPerson}>
-        <FiChevronRight />
-      </button>
-      <div>
-        <img src={image} alt={name} />
-        <h3>{name}</h3>
-        <h3>{title}</h3>
-        <p>{quote}</p>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
       </div>
-      <button onClick={previousPerson}>
-        <FiChevronLeft />
-      </button>
-      <FaQuoteRight />
-    </main>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
   );
 }
 
